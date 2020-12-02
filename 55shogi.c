7dd66b567cb3a228c310a12c025e5c4f7406a03b
Ryosuke Miyake <ryosuke@Ryosuke-mbp.local>
2020-12-02 13:40:38 +0900

    ヘッダ、マクロ定義

---
+++ 55shogi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define NONE -1
+#define FU 0
+#define GIN 1
+#define KAKU 2
+#define HISHA 3
+#define KIN 4
+#define KING 5
+#define TOKIN 6
+#define NARIGIN 7
+#define UMA 8
+#define RYU 9
+
+#define PLAYER 0   // プレイヤー
+#define AI 1       // AI
+#define NEUTRAL -1 // 中立
+
+#define FOUL_PLAY -1 // 反則
+#define VALID_PLAY 0 // 正当なプレイ
+#define CHECK_MATE 1 // 相手を詰ませた
+
+#define WILL_WIN 0    // n手先に必勝
+#define WILL_LOSE 1   // n手先に必敗
+#define EQUAL_FIGHT 2 // 互角の戦いである
+
+#define PLAYER_WIN 1
+#define AI_WIN 0
+#define CONITNUE_FIGHT -1
+
+int main()
+{
+}
